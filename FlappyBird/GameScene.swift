bd09ed0efb37f0bb4e2b59580a221b457b6b778d
harufumi.kamioka <kamioka.harufumi@gmail.com>
2018-02-13 17:29:25 +0900

    アイテム「サン」追加

---
+++ FlappyBird/GameScene.swift
@@ -0,0 +1,361 @@
+//
+//  GameScene.swift
+//  FlappyBird
+//
+//  Created by Kamioka Harufumi on 2018/02/12.
+//  Copyright © 2018年 Kamioka Harufumi. All rights reserved.
+//
+import SpriteKit
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+     var scrollNode:SKNode!
+     var wallNode:SKNode!
+     var bird:SKSpriteNode!
+    //課題
+    //sunNodeで記述してたもの
+    var itemNode:SKNode!
+    
+    let birdCategory: UInt32 = 1 << 0
+    let groundCategory: UInt32 = 1 << 1
+    let wallCategory: UInt32 = 1 << 2
+    let scoreCategory: UInt32 = 1 << 3
+    let itemCategory: UInt32 = 1 << 4 // 16
+    
+    var score = 0
+    var scoreLabelNode:SKLabelNode!
+    var bestScoreLabelNode:SKLabelNode!
+    let userDefaults:UserDefaults = UserDefaults.standard
+    
+    //課題
+    var itemScore = 0
+    var itemScoreLabelNode:SKLabelNode!
+
+     override func didMove(to view: SKView){
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -4.0)
+        physicsWorld.contactDelegate = self
+        
+        backgroundColor = UIColor(red: 0.15, green: 0.75, blue: 0.90, alpha: 1)
+        
+        scrollNode = SKNode()
+        addChild(scrollNode)
+        
+        wallNode = SKNode()
+        scrollNode.addChild(wallNode)
+        
+        //課題
+        itemNode = SKNode()
+        scrollNode.addChild(itemNode)
+
+        
+        setupGround()
+        setupCloud()
+        setupWall()
+        setupBird()
+        
+        setupScoreLabel()
+        //課題
+        setupItem()
+        setupItemScoreLabel()
+     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if scrollNode.speed > 0 {
+            bird.physicsBody?.velocity = CGVector.zero
+            bird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 15))
+        } else if bird.speed == 0 {
+            restart()
+        }
+    }
+    
+    
+    func setupGround() {
+        let groundTexture = SKTexture(imageNamed: "ground")
+        groundTexture.filteringMode = .nearest
+        
+        let needNumber = Int(self.frame.size.width / groundTexture.size().width) + 2
+
+        let moveGround = SKAction.moveBy(x: -groundTexture.size().width , y: 0, duration: 5.0)
+        
+        let resetGround = SKAction.moveBy(x: groundTexture.size().width, y: 0, duration: 0.0)
+        
+        let repeatScrollGround = SKAction.repeatForever(SKAction.sequence([moveGround, resetGround]))
+        
+        for i in 0..<needNumber {
+            let sprite = SKSpriteNode(texture: groundTexture)
+            
+            sprite.position = CGPoint(
+                x: groundTexture.size().width * (CGFloat(i) + 0.5),
+                y: groundTexture.size().height * 0.5
+            )
+            
+            sprite.run(repeatScrollGround)
+            
+            sprite.physicsBody = SKPhysicsBody(rectangleOf: groundTexture.size())
+            sprite.physicsBody?.categoryBitMask = groundCategory
+            sprite.physicsBody?.isDynamic = false
+            
+            scrollNode.addChild(sprite)
+        }
+    }
+    
+    func setupCloud() {
+        
+        let cloudTexture = SKTexture(imageNamed: "cloud")
+        cloudTexture.filteringMode = .nearest
+        
+        let needCloudNumber = Int(self.frame.size.width / cloudTexture.size().width) + 2
+        
+        let moveCloud = SKAction.moveBy(x: -cloudTexture.size().width , y: 0, duration: 20.0)
+        
+        let resetCloud = SKAction.moveBy(x: cloudTexture.size().width, y: 0, duration: 0.0)
+        
+        let repeatScrollCloud = SKAction.repeatForever(SKAction.sequence([moveCloud, resetCloud]))
+        
+        //for文の理解と計算式の理解
+        for i in 0..<needCloudNumber {
+            let sprite = SKSpriteNode(texture: cloudTexture)
+            sprite.zPosition = -100
+            
+            sprite.position = CGPoint(
+                x: cloudTexture.size().width * (CGFloat(i) + 0.5),
+                y: self.size.height - cloudTexture.size().height * 0.5
+            )
+            
+            sprite.run(repeatScrollCloud)
+            
+            scrollNode.addChild(sprite)
+        }
+    }
+    
+    func setupWall() {
+        
+        let wallTexture = SKTexture(imageNamed: "wall")
+        wallTexture.filteringMode = .linear
+        
+        let movingDistance = CGFloat(self.frame.size.width + wallTexture.size().width)
+        
+        let moveWall = SKAction.moveBy(x: -movingDistance, y: 0, duration:4.0)
+        
+        let removeWall = SKAction.removeFromParent()
+        
+        let wallAnimation = SKAction.sequence([moveWall, removeWall])
+        
+        let createWallAnimation = SKAction.run({
+            
+            let wall = SKNode()
+            wall.position = CGPoint(x: self.frame.size.width + wallTexture.size().width / 2, y: 0.0)
+            wall.zPosition = -50.0
+            
+            let center_y = self.frame.size.height / 2
+            
+            let random_y_range = self.frame.size.height / 4
+            
+            let under_wall_lowest_y = UInt32( center_y - wallTexture.size().height / 2 -  random_y_range / 2)
+            
+            let random_y = arc4random_uniform( UInt32(random_y_range) )
+        
+            let under_wall_y = CGFloat(under_wall_lowest_y + random_y)
+            
+            let slit_length = self.frame.size.height / 6
+            
+            let under = SKSpriteNode(texture: wallTexture)
+            under.position = CGPoint(x: 0.0, y: under_wall_y)
+            wall.addChild(under)
+            
+            under.physicsBody = SKPhysicsBody(rectangleOf: wallTexture.size())
+            under.physicsBody?.categoryBitMask = self.wallCategory
+            under.physicsBody?.isDynamic = false
+            
+            let upper = SKSpriteNode(texture: wallTexture)
+            upper.position = CGPoint(x: 0.0, y: under_wall_y + wallTexture.size().height + slit_length)
+            
+            upper.physicsBody = SKPhysicsBody(rectangleOf: wallTexture.size())
+            upper.physicsBody?.categoryBitMask = self.wallCategory
+            upper.physicsBody?.isDynamic = false
+            
+            wall.addChild(upper)
+            
+            let scoreNode = SKNode()
+            scoreNode.position = CGPoint(x: upper.size.width + self.bird.size.width / 2, y: self.frame.height / 2.0)
+            scoreNode.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: upper.size.width, height: self.frame.size.height))
+            scoreNode.physicsBody?.isDynamic = false
+            scoreNode.physicsBody?.categoryBitMask = self.scoreCategory
+            scoreNode.physicsBody?.contactTestBitMask = self.birdCategory
+            
+            wall.addChild(scoreNode)
+            
+            wall.run(wallAnimation)
+            
+            self.wallNode.addChild(wall)
+        })
+        
+        let waitAnimation = SKAction.wait(forDuration: 2)
+        
+        let repeatForeverAnimation = SKAction.repeatForever(SKAction.sequence([createWallAnimation, waitAnimation]))
+        
+        wallNode.run(repeatForeverAnimation)
+        
+        
+    }
+    
+    func setupBird() {
+        
+        let birdTextureA = SKTexture(imageNamed: "bird_a")
+        birdTextureA.filteringMode = .linear
+        let birdTextureB = SKTexture(imageNamed: "bird_b")
+        birdTextureB.filteringMode = .linear
+        
+        let texuresAnimation = SKAction.animate(with: [birdTextureA, birdTextureB], timePerFrame: 0.2)
+        let flap = SKAction.repeatForever(texuresAnimation)
+        
+        bird = SKSpriteNode(texture: birdTextureA)
+        bird.position = CGPoint(x: self.frame.size.width * 0.2, y:self.frame.size.height * 0.7)
+        
+        bird.physicsBody = SKPhysicsBody(circleOfRadius: bird.size.height / 2.0)
+        
+        bird.physicsBody?.allowsRotation = false
+        bird.physicsBody?.categoryBitMask = birdCategory
+        bird.physicsBody?.collisionBitMask = groundCategory | wallCategory
+        bird.physicsBody?.contactTestBitMask = groundCategory | wallCategory
+        
+        bird.run(flap)
+        
+        addChild(bird)
+    }
+    
+    func setupItem() {
+        let itemSunTexture = SKTexture(imageNamed: "item_sun")
+        itemSunTexture.filteringMode = SKTextureFilteringMode.linear
+        
+        let movingDistance = CGFloat(self.frame.size.width + itemSunTexture.size().width)
+        let moveItemSun = SKAction.moveBy(x: -movingDistance ,y: 0 ,duration: 4.0)
+        let removeItemSun = SKAction.removeFromParent()
+        let itemSunAnimation = SKAction.sequence([moveItemSun, removeItemSun])
+        
+        let createItemSunAnimation = SKAction.run({
+            let sun = SKNode()
+            sun.physicsBody = SKPhysicsBody(circleOfRadius: itemSunTexture.size().height / 2.0)
+            sun.physicsBody?.isDynamic = false
+            //アイテムの範囲設定。壁と壁の間に出現させたい。
+            let sun_x_appear = self.frame.size.width - itemSunTexture.size().width
+            let random_sun_x = arc4random_uniform(UInt32(sun_x_appear))
+            let sun_x = CGFloat(random_sun_x)
+            
+            let center_y = self.frame.size.height / 2
+            let random_sun_range = self.frame.size.height / 4
+            let under_sun_lowest_y = UInt32( center_y - itemSunTexture.size().height / 2 - random_sun_range / 2)
+            let random_sun_y = arc4random_uniform( UInt32(random_sun_range))
+            let under_sun_y = CGFloat(under_sun_lowest_y + random_sun_y)
+            let sun_y = CGFloat(random_sun_y)
+            
+            var itemSun = SKSpriteNode(texture: itemSunTexture)
+            sun.position = CGPoint(x: sun_x, y: under_sun_y + itemSunTexture.size().height + sun_y
+            )
+            
+            itemSun.physicsBody?.categoryBitMask = self.itemCategory
+            itemSun.physicsBody?.contactTestBitMask = self.birdCategory
+            
+            sun.addChild(itemSun)
+            
+            sun.run(itemSunAnimation)
+            
+            self.itemNode.addChild(sun)
+            
+            
+        })
+        
+        let waitAnimation = SKAction.wait(forDuration: 3)
+        
+        let repeatForeverAnimation = SKAction.repeatForever(SKAction.sequence([createItemSunAnimation, waitAnimation]))
+        
+        itemNode.run(repeatForeverAnimation)
+    }
+    
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        if scrollNode.speed <= 0 {
+            return
+        }
+        
+        if (contact.bodyA.categoryBitMask & scoreCategory) == scoreCategory || (contact.bodyB.categoryBitMask & scoreCategory) == scoreCategory {
+            print("ScoreUp")
+            score += 1
+            scoreLabelNode.text = "Score:\(score)"
+            
+           var bestScore = userDefaults.integer(forKey: "BEST")
+            if score > bestScore {
+                bestScore = score
+                bestScoreLabelNode.text = "Best Score:\(bestScore)"
+                userDefaults.set(bestScore, forKey: "BEST")
+                userDefaults.synchronize()
+            }
+            //}がなかった。
+        } else {
+            print("GameOver")
+            
+            scrollNode.speed = 0
+            
+            bird.physicsBody?.collisionBitMask = groundCategory
+            
+            let roll = SKAction.rotate(byAngle: CGFloat(Double.pi) * CGFloat(bird.position.y) * 0.01, duration:1)
+            bird.run(roll, completion:{
+                self.bird.speed = 0
+            })
+        }
+    
+    }
+
+    
+   func restart() {
+        score = 0
+        scoreLabelNode.text = String("Score:\(score)")
+        
+        bird.position = CGPoint(x: self.frame.size.width * 0.2, y:self.frame.size.height * 0.7)
+        bird.physicsBody?.velocity = CGVector.zero
+        bird.physicsBody?.collisionBitMask = groundCategory | wallCategory
+        bird.zRotation = 0.0
+        
+        wallNode.removeAllChildren()
+        
+        bird.speed = 1
+        scrollNode.speed = 1
+    }
+    
+
+    func setupScoreLabel() {
+        score = 0
+        scoreLabelNode = SKLabelNode()
+        scoreLabelNode.fontColor = UIColor.black
+        scoreLabelNode.position = CGPoint(x: 10, y: self.frame.size.height - 60)
+        scoreLabelNode.zPosition = 100
+        scoreLabelNode.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.left
+        scoreLabelNode.text = "Score:\(score)"
+        self.addChild(scoreLabelNode)
+    
+        bestScoreLabelNode = SKLabelNode()
+        bestScoreLabelNode.fontColor = UIColor.black
+        bestScoreLabelNode.position = CGPoint(x: 10, y: self.frame.size.height - 90)
+        bestScoreLabelNode.zPosition = 100
+        bestScoreLabelNode.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.left
+    
+        let bestScore = userDefaults.integer(forKey: "BEST")
+        bestScoreLabelNode.text = "Best Score:\(bestScore)"
+        self.addChild(bestScoreLabelNode)
+    }
+    
+    func setupItemScoreLabel() {
+        itemScore = 0
+        itemScoreLabelNode = SKLabelNode()
+        itemScoreLabelNode.fontColor = UIColor.black
+        itemScoreLabelNode.position = CGPoint(x: 10, y: self.frame.size.height - 60)
+        itemScoreLabelNode.zPosition = 100
+        itemScoreLabelNode.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.left
+        itemScoreLabelNode.text = "Item Score:\(itemScore)"
+        self.addChild(itemScoreLabelNode)
+        
+    }
+    
+
+}
+
